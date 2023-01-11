8cf8c813c545d6aacefd782a63bbe3c21d1fbc28
Volodymyr Polishchuk <volodimirpolishchuck@gmail.com>
2023-01-11 17:17:35 +0200

    first version - (needs corrections)

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,9 +1,8 @@
 package org.project;
 
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) {
 
     }
 }
